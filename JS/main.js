38e277418c54a638215aa7b6658c5f6525b660aa
evalopezm <evalopezm>
2020-03-20 10:51:07 +0100

    paint products not completedfor checking out

--- JS/main.js
+++ JS/main.js
@@ -3,16 +3,35 @@
 //global constants
 const searchButton = document.querySelector('.search-btn');
 const userSearch = document.querySelector('.user-search');
+const foundSerie = document.querySelector('.serie-card');
 
+let series = [];
 
 //Read input and call the API
 function collectSearch () {
-    let wordToSearch = userSearch.value;
-    fetch (`http://api.tvmaze.com/search/shows?q=:${wordToSearch}`)
+    fetch (`http://api.tvmaze.com/search/shows?q=:${userSearch.value}`)
     .then (response => response.json())
     .then (data => {
         console.log (data);
+        series = data;
+    paintFoundSeries;
     })
+}
+collectSearch();
+
+// for series [i] (i=0, i < series.length, i++) {
+
+// };
+
+//Paint series
+const paintFoundSeries = () => {
+    let serieData = '';
+    for (const serie of series) {
+        serieData += `<article class='found-serie'>`
+        serieData += `<h2 class='serie-title'>${serie[i].show.name}</h2>`
+        serieData += `<img class='serie-img' src='${serie[i].show.image.medium}' alt='${serie[i].show.name}'>`
+    }
+    foundSerie.innerHTML = serieData;
 }
 
 
