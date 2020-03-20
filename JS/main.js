636eb77f37ff602b8e6c49e15648002d3c103a31
evalopezm <evalopezm>
2020-03-20 18:27:34 +0100

    add placeholder image

--- JS/main.js
+++ JS/main.js
@@ -5,6 +5,8 @@ const searchButton = document.querySelector('.search-btn');
 const userSearch = document.querySelector('.user-search');
 const foundSerie = document.querySelector('.serie-card');
 
+
+
 let series = [];
 
 //Read input and call the API
@@ -12,25 +14,28 @@ function collectSearch () {
     fetch (`http://api.tvmaze.com/search/shows?q=:${userSearch.value}`)
     .then (response => response.json())
     .then (data => {
-        // console.log (data);
-        series = data;
+        series = data;    
+        // console.log(series);
+        
     paintFoundSeries ();
     })
 }
+
 collectSearch();
 
-// for series [i] (i=0, i < series.length, i++) {
-// };
 
-//Paint series
+// Paint series
 function paintFoundSeries () {
     let serieData = '';
     for (const serie of series) {
-        console.log(serie.show.name);
-        
         serieData += `<article class='found-serie'>`
         serieData += `<h2 class='serie-title'>${serie.show.name}</h2>`
-        serieData += `<img class='serie-img' src='${serie.show.image.medium}' alt='${serie.show.name}'>`
+        if (serie.show.image === null){
+            serieData += `<img class='serie-img' src='https://via.placeholder.com/210x295/ffffff/666666?text=Best+TV+Series' alt='${serie.show.name}'>`
+        }
+        else {
+            serieData += `<img class='serie-img' src='${serie.show.image.medium}' alt='${serie.show.name}'>`
+        }
         serieData +=`</article>`
     }
     foundSerie.innerHTML = serieData;
