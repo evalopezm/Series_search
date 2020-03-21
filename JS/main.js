0802355e218881a9907a2c705d81f34b48722336
evalopezm <evalopezm>
2020-03-21 21:37:23 +0100

    listen favs create array and first try to paint favs

--- JS/main.js
+++ JS/main.js
@@ -3,11 +3,12 @@
 //global constants
 const searchButton = document.querySelector('.search-btn');
 const userSearch = document.querySelector('.user-search');
-const foundSerie = document.querySelector('.serie-card');
-
+const foundSeries = document.querySelector('.found-series');
+const favouriteSeries = document.querySelector('.js-favourite-serie-card');
 
 
 let series = [];
+let favourites = [];
 
 //Read input and call the API
 function collectSearch () {
@@ -16,11 +17,9 @@ function collectSearch () {
     .then (data => {
         series = data;    
         // console.log(series);
-        
     paintFoundSeries ();
     })
 }
-
 collectSearch();
 
 
@@ -28,35 +27,78 @@ collectSearch();
 function paintFoundSeries () {
     let serieData = '';
     for (const serie of series) {
-        serieData += `<article class='found-serie'>`
+        serieData += `<article class='serie-card'>`
         serieData += `<h2 class='serie-title'>${serie.show.name}</h2>`
+        //Resolve lost pictures 
         if (serie.show.image === null){
-            serieData += `<img class='serie-img' src='https://via.placeholder.com/210x295/ffffff/666666?text=Best+TV+Series' alt='${serie.show.name}'>`
+            serieData += `<img class='serie-img' src='https://via.placeholder.com/210x295/ffffff/666666?text=Best+TV+Series' alt='${serie.show.name}' id='${serie.show.id}'>`
         }
         else {
-            serieData += `<img class='serie-img' src='${serie.show.image.medium}' alt='${serie.show.name}'>`
+            serieData += `<img class='serie-img' src='${serie.show.image.medium}' alt='${serie.show.name}' id='${serie.show.id}'>`
         }
         serieData +=`</article>`
     }
-    foundSerie.innerHTML = serieData;
+    foundSeries.innerHTML = serieData;
+    ListenSelectedSerie ();
 }
+
+//Add to favourites
+function addSerieToFavourites (ev) {
+    //identify clicked element id
+    const clickedSerie = ev.target.id;
+    //Asociate id with element from array
+    let selectedSerie;
+    for (const serie of series) { 
+        if (serie.show.id == clickedSerie) {
+            selectedSerie = serie;
+        }
+    }
+    //add to favourites array
+    favourites.push({
+        name: selectedSerie.show.name,
+        image: selectedSerie.show.image.medium,
+        id: selectedSerie.show.id
+    });
+    paintFavouriteSeries ();
+};
+
+const getFavouriteHtmlCode = favourite => {
+    let favouriteHtmlCode = '';
+    favouriteHtmlCode += `<li>`;
+    favouriteHtmlCode += `<p>${favourite.name}</p>`;
+    favouriteHtmlCode += `<img class='favourite-img' src='${favourite.image}' alt='${favourite.name}'>`;
+    favouriteHtmlCode += `</li>`;
+}
+
+const paintFavouriteSeries = () => {
+    debugger;
+    favouriteSeries.innerHTML = '';
+    for (const favourite of favourites) {
+        favouriteSeries.innerHTML += getFavouriteHtmlCode(favourite);
+    }
+}
+
+
 
 
 //listen search button
 searchButton.addEventListener ('click', collectSearch);
-
-
-
-
-
 //listen click in one serie
+const ListenSelectedSerie = () => {
+    const seriesCards = document.querySelectorAll('.serie-img');
+    for (const serieCard of seriesCards) {
+        serieCard.addEventListener('click', addSerieToFavourites);
+    }
+}
+
+
+
+
+
 //change css
-//add to favourites
 //If series is in favourites, else....
 
 //localStorage keep
 //localStorage read
 //localStorage paint
-
-
-//others: Resolve lost pictures; reset button 
+//Others: reset button
